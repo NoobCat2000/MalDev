02fa5485ba627365c7280bf66d4a2700b484bb30
Your <you@gmail.com>
2024-11-07 16:19:24 +0700

    from laptop 53

--- TestCpp/main.c
+++ TestCpp/main.c
@@ -2,18 +2,7 @@
 #include <stdio.h>
 
 int main() {
-	CHAR szFlag[0x100];
-
-	SecureZeroMemory(szFlag, sizeof(szFlag));
-	puts("Nhap dap an:");
-	fgets(szFlag, _countof(szFlag), stdin);
-	szFlag[lstrlenA(szFlag) - 1] = '\0';
-	if (!lstrcmpA(szFlag, "Lop PTMD")) {
-		puts("Dap an dung");
-	}
-	else {
-		puts("Dap an sai");
-	}
+	MessageBoxA(NULL, "Hello World", "Title", MB_OK);
 
 	return 0;
 }
